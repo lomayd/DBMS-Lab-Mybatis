0b5cc8dce6abe2bbf7a5dbf01d4e60b7725c1f46
lomayd <jinho10167@gmail.com>
2023-05-11 06:59:13 +0000

    feat: Project Initialize

---
+++ src/main/java/lomayd/DBMSLabMybatis/api/DbmsLabMybatisApplication.java
@@ -0,0 +1,13 @@
+package lomayd.DBMSLabMybatis.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsLabMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsLabMybatisApplication.class, args);
+	}
+
+}
